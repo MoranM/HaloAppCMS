debc88a3e40c472145190e827cc6ef79f553b845
MoranM <mishanmo@gmail.com>
2014-09-04 09:18:15 +0300

    image picking styling

--- public/js/story.js
+++ public/js/story.js
@@ -1,9 +1,38 @@
+
 /**
  * Created by L&M on 03/09/14.
  */
 
-$(function(){
-   $("#get-all-images-btn").on("click", function(){
-       $.getJSON("/story/get-all-authors-images")
-   })
+$(function () {
+    initAuthorsImageSectionBox();
+
 });
+
+function initAuthorsImageSectionBox() {
+    var wasCalled = false;
+    var formScope = $("#upload-story");
+
+    $("#get-all-images-btn").on("click", function () {
+        var modal = $("#pick-image-modal");
+        var modalBody = modal.find(".modal-body");
+
+        if (!wasCalled) {
+            $.getJSON("/story/get-all-authors-images")
+                .done(function (images) {
+                var len = images.length;
+                for (var i = 0; i < len; i++) {
+                    var src = images[i].authorImageUrl;
+                    modalBody.append($("<div></div>").addClass("image-wrapper").html('<img class="img-rounded" src=' + src + ' />'));
+                }
+
+                modal.on("click", "img", function(){
+                    var imageSrc = $(this).attr("src");
+                    formScope.find(".selected-image-container img").attr("src", imageSrc);
+                    modal.modal("hide");
+                });
+                wasCalled = true;
+            });
+        }
+        modal.modal();
+    });
+}
